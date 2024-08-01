dce2c5ce7406f8e6f14cdd88e4d10b10f14058b9
MauricioStrang <strangmauricio@gmail.com>
2024-08-01 20:18:27 -0300

    catphoto app

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,14 @@
 import styles from "./page.module.css";
 
+//teaching Ferchu the basics,
+//he seems to have problems opening his project, aparently by not understanding what the main body of the projects is
+//good tag understanding
+
 export default function Home() {
   return (
       <div>
-        
+         <h1>CatPhoto App</h1>
+
       </div>
   );
 }
