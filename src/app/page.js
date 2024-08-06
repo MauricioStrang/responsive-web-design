23de7b3761ba4859ac0af62ccec60aebd9bca905
MauricioStrang <strangmauricio@gmail.com>
2024-08-05 23:09:23 -0300

    cat ul li

--- src/app/page.js
+++ src/app/page.js
@@ -8,8 +8,15 @@ export default function Home() {
   return (
       <div>
          <h1>CatPhoto App
-          
-
+          <p>Cats are really agile</p>
+          <ul>
+            <li>
+              cats are dumb
+            </li>
+            <li>
+              cats are cute
+            </li>
+          </ul>
          </h1>
          <img src="cat.jpg" alt=""></img>
 
