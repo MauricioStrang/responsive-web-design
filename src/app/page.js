9a14e680ac227a64d1f0e90fe307a5b65987ac1c
MauricioStrang <strangmauricio@gmail.com>
2024-08-08 20:31:41 -0300

    cats loves and hates

--- src/app/page.js
+++ src/app/page.js
@@ -7,11 +7,43 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
       <div className={styles.container}>
-        <h1>CatPhotoApp</h1>
-        <h2>Cat Photos</h2>
-        <p>See more <a href="https://freecatphotoapp.com" target="_blank">cat photos</a> in our gallery.</p>
-        <img src="https://cdn.freecodecamp.org/curriculum/cat-photo-app/relaxing-cat.jpg" alt=""/>
-        <ul></ul>
+
+        <section>
+          <h1>CatPhotoApp</h1>
+          <h2>Cat Photos</h2>
+          <p>See more <a href="https://freecatphotoapp.com" target="_blank">cat photos</a> in our gallery.</p>
+          <a href="https://freecatphotoapp.com"><img src="https://cdn.freecodecamp.org/curriculum/cat-photo-app/relaxing-cat.jpg" alt=""/></a>
+        </section>
+
+        <section>
+          <h2>
+            Cat Lists
+          </h2>
+          <h3>
+            Things cats love:
+          </h3>
+          <ul>
+            <li>cat nip</li>
+            <li>laser pointer</li>
+            <li>lasgna</li>
+          </ul>
+          <figure>
+            <img src="https://cdn.freecodecamp.org/curriculum/cat-photo-app/lasagna.jpg" alt="A slice of lasagna on a plate."/>
+            <figcaption>Cats <em>love</em> lasagna</figcaption>
+          </figure>
+          <h3>Top 3 things cat hate:</h3>
+          <ul>
+            <li>flea treatment</li>
+            <li>thunder</li>
+            <li>other cats</li>
+          </ul>
+          <figure>
+            <img src="https://cdn.freecodecamp.org/curriculum/cat-photo-app/cats.jpg" alt="Five cats looking around a field."/>
+            <figcaption>Cats <strong>hate</strong> other cats.</figcaption>
+          </figure>
+        
+        </section>
+        
       </div>
   );
 }
